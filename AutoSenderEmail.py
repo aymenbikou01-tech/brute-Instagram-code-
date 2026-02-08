59f8f4d4ba70b9566714c3b04b7a8c8dc2fcee37
404 <aymenbikou01@gmail.com>
2026-02-08 17:27:18 +0100

    Auto send mail message
    Updated email configuration and message file path.

--- AutoSenderEmail.py
+++ AutoSenderEmail.py
@@ -11,9 +11,9 @@ print(Fore.CYAN + f.renderText('SendEmail-404') + Style.RESET_ALL)
 
 
 SENDER_EMAIL = "i#####@gmail.com"#your emial phishing
-APP_PASSWORD = "##pppo09709##"#app pass
+APP_PASSWORD = "##pppo09709##"#AppPass 
 RECEIVER_EMAIL = input("emial phishin: ")
-HTML_FILE = input('Message file html:')
+HTML_FILE = "message-E-mail-Phishin for instagram.html"
 
 
 with open(HTML_FILE, "r", encoding="utf-8") as f:
@@ -31,4 +31,4 @@ with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
     server.login(SENDER_EMAIL, APP_PASSWORD)
     server.send_message(msg)
 
-print("[+] Attack started ....")
+print("[+] message send (finish the attack ) ....")
